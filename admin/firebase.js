aeb138ec3288be4233a93774b4de56ef4b333e50
PaulAntony <paulantony7722@gmail.com>
2026-02-24 22:45:16 +0530

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase CDN imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBMYyLQAu50cUHuXpHrYartoL2hr2nLiMo",
   authDomain: "ieee-visat-website.firebaseapp.com",
   projectId: "ieee-visat-website",
   storageBucket: "ieee-visat-website.firebasestorage.app",
   messagingSenderId: "713892043284",
-  appId: "1:713892043284:web:97b92a656ca016d3712501",
-  measurementId: "G-YD7NYJJV1Z"
+  appId: "1:713892043284:web:97b92a656ca016d3712501"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
